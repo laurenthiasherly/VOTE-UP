0b77a645a2e5ca257287ce13c757d2573129331d
Laurenthia S <laurenthiasherly@gmail.com>
2017-03-25 22:59:02 -0700

    Add files via upload

---
+++ partials/header.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+         <title>VANARTAS STUDENT MOCK PROJECT SITE - MARK YOUR VOTE UP</title>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <meta name="description" content="This is a student exercise website for the Vancouver Institute of Media Arts (www.vanarts.com)">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/main.css">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+        
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/vendor/foundation.min.js" type="text/javascript"></script>
+        
+    </head>
+    
+    <body>
+        
+        
+        <header class="page-header column large-12 medium-12 small-12 padding-none">
+            <nav class="column large-12 show-for-large padding-none">
+                <ul class="navigation center">
+                    <li class="column large-1 medium-1 small-9 nav">                       
+                        <a href="index.php"> 
+                            <img src="img/homepage/logo-white.png" alt="the logo" 
+                                 class="nav-logo">
+                        </a>
+                    </li>
+                    <li class="column large-2 medium-2 nav nav-conservative">                       
+                        <a href="conservative.php" id="nav-conservative"> Conservative </a>
+                    </li>
+                      <li class="column large-2 medium-2 nav nav-democratic">                       
+                        <a href="democratic.php" id="nav-democratic"> Democratic </a>
+                    </li>
+                    <li class="column large-2 medium-2 nav nav-green">                       
+                        <a href="green.php" id="nav-green"> Green </a>
+                    </li>
+                    <li class="column large-2 medium-2 nav nav-liberal">                       
+                        <a href="liberal.php" id="nav-liberal"> Liberal </a>
+                    </li>
+                    <li class="column large-2 medium-2 nav1">                       
+                        <a href="mp.php"> Who's my MP </a>
+                    </li>
+                    <li class="column large-1 medium-1 nav2">                       
+                        <a id="subscribe-button"> Subscribe </a>
+                    </li>
+                    
+                </ul>   
+            </nav>
+            
+            
+            <div class="column large-12 medium-12 large-12 padding-vertical center" id="subscribe-box">
+                <div class="column large-2 show-for-large padding-none center ">
+                    <h4> Subscribe for Updated News! </h4>
+
+                </div>
+
+                <div class="column large-10 medium-12 padding-none center ">
+
+                    <form method='post' action='' >
+
+                        <ul class="subscription center">
+                            <li>
+                                <label for="theName"> Name </label>
+                                <input type="text" name="name" id="theName"
+                                       />     
+                            </li>
+                            <li>
+                                <label for="theEmail"> Email </label>
+                                <input type="text" name="email" id="theEmail"
+                                       />     
+                            </li>
+                            <li>
+                                <input type="submit" name="subscribe" value="Subscribe"
+                                       class="button"/>     
+                            </li>
+
+                    </form>            
+                </div>
+
+
+            </div>
+    
+            
+            
+            <div class="column medium-12 small-12 padding-none center hide-for-large">
+                <nav>
+                    <ul class="navigation">
+                        <li class="column large-4 medium-1 small-9 nav">                       
+                            <a href="index.php"> 
+                                <img src="img/homepage/logo-white.png" alt="the logo" 
+                                     class="nav-logo-mobile">
+                            </a>
+                        </li>
+                        <li>
+                            <a class="mobile-menu large-8">
+                                <i class="fa fa-bars fa-2x" aria-hidden="true">
+                            </i></a>
+                        </li>
+                    </ul>    
+                </nav>
+            </div>
+            
+            <div id="menu-control" class="column hide-for-large medium-12 small-12 padding-none">
+                <ul class="navigation-mobile center padding-none">
+
+                    <li class="column medium-12 small-12 padding-vertical nav-conservative">                       
+                        <a href="conservative.php" id="nav-conservative"> Conservative </a>
+                    </li>
+                      <li class="column medium-12 small-12 padding-vertical nav-democratic">                       
+                        <a href="democratic.php" id="nav-democratic"> Democratic </a>
+                    </li>
+                    <li class="column medium-12 small-12 padding-vertical nav-green">                       
+                        <a href="green.php" id="nav-green"> Green </a>
+                    </li>
+                    <li class="column medium-12 small-12 padding-vertical nav-liberal">                       
+                        <a href="liberal.php" id="nav-liberal"> Liberal </a>
+                    </li>
+                    <li class="column medium-12 small-12 padding-vertical nav1-mobile">                       
+                        <a href="mp.php"> Who's my MP </a>
+                    </li>
+                    <li class="column medium-12 small-12 padding-vertical nav2-mobile">                       
+                        <a href="subscribe.php"> Subscribe </a>
+                    </li>
+                    
+                </ul>   
+                
+            </div>
+        </header>
+        
+        
+        
---
+++ partials/party.php
@@ -0,0 +1,84 @@
+
+
+<?php include "partials/header.php"; ?>
+
+
+    <div class="side-topics-bar">
+        <ul class="sidebar-topic-style">
+            <li class="sidebar-topic" data-direct="scroll1"><a>1</a></li>
+            <li class="sidebar-topic" data-direct="scroll2"><a>2</a></li>
+            <li class="sidebar-topic" data-direct="scroll3"><a>3</a></li>
+            <li class="sidebar-topic" data-direct="scroll4"><a>4</a></li>
+            <li class="sidebar-topic" data-direct="scroll5"><a>5</a></li>
+            <li class="sidebar-topic" data-direct="scroll6"><a>6</a></li>
+            <li class="sidebar-topic" data-direct="scroll7"><a>7</a></li>
+            <li class="sidebar-topic" data-direct="scroll8"><a>8</a></li>
+            <li class="sidebar-topic" data-direct="scroll9"><a>9</a></li>
+            <li id="filterShow" class="sidebar-topic"><a>F</a></li>
+        </ul>
+        <div class="tip-topic-style">
+            <div class="tip-topic"><a class="tip-style">Economy</a></div>
+            <div class="tip-topic"><a class="tip-style">Environment</a></div>
+            <div class="tip-topic"><a class="tip-style">Education</a></div>
+            <div class="tip-topic"><a class="tip-style">Health</a></div>
+            <div class="tip-topic"><a class="tip-style">Immigration</a></div>
+            <div class="tip-topic"><a class="tip-style">Housing</a></div>
+            <div class="tip-topic"><a class="tip-style">Privacy</a></div>
+            <div class="tip-topic"><a class="tip-style">Foreign Policy</a></div>
+            <div class="tip-topic"><a class="tip-style">Electoral Reform</a></div>
+            <div class="tip-topic"><a class="tip-style">Filter</a></div>
+        </div>
+    </div>
+
+    <div id="scroll1" class="first-content">
+    </div>
+    <div id="scroll2" class="second-content">
+    </div>
+    <div id="scroll3" class="third-content">
+    </div>
+    <div id="scroll4" class="fourth-content">
+    </div>
+    <div id="scroll5" class="fith-content">
+    </div>
+    <div id="scroll6" class="first-content">
+    </div>
+    <div id="scroll7" class="second-content">
+    </div>
+    <div id="scroll8" class="third-content">
+    </div>
+    <div id="scroll9" class="fourth-content">
+    </div>
+
+
+    <div id="modalBox" class="modal-box-comparing">
+        <div class="comparing-box">
+            <a id="closeComparingBox">X</a>
+            <div class="comp-topbar-style">
+                <ul>
+                    <li data-topic="1" data-selected="0" class="party-top-comp">Liberal</li>
+                    <li data-topic="1" data-selected="0" class="party-top-comp">Conservative</li>
+                    <li data-topic="1" data-selected="0" class="party-top-comp">Democratic</li>
+                    <li data-topic="1" data-selected="0" class="party-top-comp">Green</li>
+                </ul>
+            </div>
+            <div class="comp-sidebar-topic">
+                <ul class="comp-topic-style">
+                    <li data-topic="1" data-selected="0" class="sidebar-topic-filt"><a>1</a></li>
+                    <li data-topic="2" data-selected="0" class="sidebar-topic-filt"><a>2</a></li>
+                    <li data-topic="3" data-selected="0" class="sidebar-topic-filt"><a>3</a></li>
+                    <li data-topic="4" data-selected="0" class="sidebar-topic-filt"><a>4</a></li>
+                    <li data-topic="5" data-selected="0" class="sidebar-topic-filt"><a>5</a></li>
+                    <li data-topic="6" data-selected="0" class="sidebar-topic-filt"><a>6</a></li>
+                    <li data-topic="7" data-selected="0" class="sidebar-topic-filt"><a>7</a></li>
+                    <li data-topic="8" data-selected="0" class="sidebar-topic-filt"><a>8</a></li>
+                    <li data-topic="9" data-selected="0" class="sidebar-topic-filt"><a>9</a></li>
+                </ul>
+            </div>
+            <div class="comp-content">
+                <div id="contentTopic">
+                    <p class="content-topic-default">Please select the parties and Topics.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  
