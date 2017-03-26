eaa559706b4d6eaf8a7f9f2385722f3319ad3c3b
YOS <yuki8963@gmail.com>
2017-03-26 02:56:32 -0700

    changed main.js file

--- js/main.js
+++ js/main.js
@@ -6,49 +6,58 @@ var movingScroll=false;
 var animationStatus=false;
 var iScrollPos = 0;
 
+//Fix1
+var sizeOfScrollDiv=0;
+
+
 $(function(){
     
-    
-    
+    //Fix1
+    sizeOfScrollDiv=$(".scrollDiv").length;
+   
     resize();
     
-    
-    
-    
-    
-    
     $(this).scrollTop(0);
-    if($( window ).width()>1024){
-        document.body.addEventListener("wheel", myFunction);
-    }
     
+    
+    
+    document.body.addEventListener("wheel", myFunction);
     
     function myFunction(size) {
-   
+        getpositionActual();
         if(size.wheelDelta<0){
+            
             if(!movingScroll){
                 changeCurrent+=1;
-                var target = $("#scroll"+changeCurrent);
-                if (target.length) {
-                    if(!animationStatus){
-                        movingScroll=true;
-                        animationStatus=true;
-                        $('body').animate({scrollTop: target.offset().top}, 1000,scrollAnimationFinished);
+                //Fix1
+                if(changeCurrent<=sizeOfScrollDiv){
+                    var target = $("#scroll"+changeCurrent);
+                    if (target.length) {
+                        if(!animationStatus){
+                            movingScroll=true;
+                            animationStatus=true;
+                            $('body').animate({scrollTop: target.offset().top}, 1000).delay(200).animate({},scrollAnimationFinished);
+                        }
                     }
+                    //Fix1
+                }else{
+                    changeCurrent=sizeOfScrollDiv;
                 }
             }
         }else{
             if(!movingScroll){
                 changeCurrent-=1;
+                //Fix1
                 if(changeCurrent<=0){
-                    changeCurrent=0;
-                }
-                var target = $("#scroll"+changeCurrent);
-                if (target.length) {
-                    if(!animationStatus){
-                        movingScroll=true;
-                        animationStatus=true;
-                        $('body').animate({scrollTop: target.offset().top}, 1000,scrollAnimationFinished);
+                    changeCurrent=1;
+                }else{
+                    var target = $("#scroll"+changeCurrent);
+                    if (target.length) {
+                        if(!animationStatus){
+                            movingScroll=true;
+                            animationStatus=true;
+                            $('body').animate({scrollTop: target.offset().top}, 1000).delay(200).animate({},scrollAnimationFinished);
+                        }
                     }
                 }
             }
@@ -124,31 +133,12 @@ $(function(){
     $.each($(".nav"),function(index){
         $(".nav-"+navParty[index]).hover(function(){
             $("#nav-"+navParty[index]).css("border-top","solid 2.5px "+navColour[index]);
-            //$("#nav-"+navParty[index]).css("border-bottom","solid 2.5px "+navColour[index]);
             $("#nav-"+navParty[index]).css("padding","10px 20px 0 20px");
 
         },function(){
             $("#nav-"+navParty[index]).css("border-top","none");
-            $("#nav-"+navParty[index]).css("border-bottom","none");
         });
     });
-    
-    menuShow =0;
-    $(".mobile-menu").click(function(){
-        if(menuShow==0){
-            $(".navigation-mobile").css("top","50px");
-            menuShow=1;
-
-        }else{
-            $(".navigation-mobile").css("top","-300px");
-            menuShow=0;
-        }
-    });
-
-    
-    
-    
-    
     
     
     
@@ -195,59 +185,35 @@ $(function(){
     
     
     $(".homepage-orange").mouseover(function(){
-        $(".homepage-orange-hover").css("transform","translate(-100%,0)");  
-        $(".arrow-democratic").css({"display":"initial", "margin-top":"80px"});
-        $("#logo-democratic").css("margin-top","12vh");
-        //$(".arrow-parties").slideDown({"display":"initial"},1000,"swing");
+        $(".homepage-orange-hover").css("transform","translate(-100%,0)");          
     });    
     $(".homepage-orange").mouseout(function(){
-        $(".homepage-orange-hover").css("transform","translate(0,0)");
-        $(".arrow-parties").css("display","none");
-        $("#logo-democratic").css("margin-top","18vh");
-        //$(".arrow-parties").slideUp({"display":"none"},1000,"swing");
+        $(".homepage-orange-hover").css("transform","translate(0,0)");              
     });
     
     
     $(".homepage-blue").mouseover(function(){
-        $(".homepage-blue-hover").css("transform","translate(-100%,0)");  
-        $(".arrow-conservative").css({"display":"initial", "margin-top":"80px"});
-        $("#logo-conservative").css("margin-top","12vh");
+        $(".homepage-blue-hover").css("transform","translate(-100%,0)");          
     });    
     $(".homepage-blue").mouseout(function(){
-        $(".homepage-blue-hover").css("transform","translate(0,0)"); 
-        $(".arrow-parties").css("display","none");
-        $("#logo-conservative").css("margin-top","18vh");
+        $(".homepage-blue-hover").css("transform","translate(0,0)");              
     });
     
     
     $(".homepage-green").mouseover(function(){
-        $(".homepage-green-hover").css("transform","translate(100%,0)");  
-        $(".arrow-green").css({"display":"initial", "margin-top":"80px"});
-        $("#logo-green").css("margin-top","12vh");
+        $(".homepage-green-hover").css("transform","translate(100%,0)");          
     });    
     $(".homepage-green").mouseout(function(){
-        $(".homepage-green-hover").css("transform","translate(0,0)");
-        $(".arrow-parties").css("display","none");
-        $("#logo-green").css("margin-top","18vh");
+        $(".homepage-green-hover").css("transform","translate(0,0)");              
     });
     
     
     $(".homepage-red").mouseover(function(){
-        $(".homepage-red-hover").css("transform","translate(100%,0)");   
-        $(".arrow-liberal").css({"display":"initial", "margin-top":"80px"});
-        $("#logo-liberal").css("margin-top","12vh");
+        $(".homepage-red-hover").css("transform","translate(100%,0)");          
     });    
     $(".homepage-red").mouseout(function(){
-        $(".homepage-red-hover").css("transform","translate(0,0)");   
-        $(".arrow-parties").css("display","none");
-        $("#logo-liberal").css("margin-top","18vh");
+        $(".homepage-red-hover").css("transform","translate(0,0)");              
     });
-    
-    
-    
-    
-    
-    
     // ------------ THE VIDEO -----------
     
     window.onresize = function(event){
@@ -255,104 +221,26 @@ $(function(){
     }
     
     function resize(){
-        //$("#my-video").css("width", window.innerWidth);
-        //$("#my-video").css("height", window.innerHeight);
+        $("#my-video").css("width", window.innerWidth);
+        $("#my-video").css("height", window.innerHeight);
         $("#my-video").css("min-height", window.innerHeight);
+          
         
         var boxHeight = $(".homepage-orange").height() + 20;
         //alert(boxHeight);
         $(".party-div-height").css("height",boxHeight);
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // ====================================================
-    //                  CONSERVATIVE
-    // ===================================================
-
-    function conservativeDesc(){
-        
-        if(window.innerWidth > 1024){
-            setInterval(function(){
-
-                $(".conservative-desc-para").css("margin-right","0");
-                $(".conservative-desc-title").css("width","50%");
-
-
-            },2500);
-        }
-    }
-    
-    conservativeDesc();
-    
-    
-    $(".voting-icons").each(function(){
-        $(".voting-icons").hover(function(){
-            $(".voting-icons").css("width","120px");
-            
-        },function(){
-            $(".voting-icons").css("width","100px");
-            
-        });
-    });
-    
-    (function($) {
-        $.fn.clickToggle = function(func1, func2) {
-            var funcs = [func1, func2];
-            this.data('toggleclicked', 0);
-            this.click(function() {
-                var data = $(this).data();
-                var tc = data.toggleclicked;
-                $.proxy(funcs[tc], this)();
-                data.toggleclicked = (tc + 1) % 2;
-            });
-            return this;
-        };
-    }(jQuery));
-    
-    $(".voting-icons").clickToggle(function(){
-        $(".voting-icons").attr('src','img/conservative/badge-icon-clicked.svg');
-        $(".voted").css("margin-top","0");
-        $(".voting-icons").css("width","120px");
-
-    },function(){
-        $(".voting-icons").attr('src','img/conservative/badge-icon.svg');
-        $(".voted").css("margin-top","800px");
-        $(".voting-icons").css("width","100px");
-    });
-    
-    
-    
-    $(".voting-icons-mediumsmall").clickToggle(function(){
-        $(".voting-icons-mediumsmall").attr('src','img/conservative/badge-icon-clicked.svg');
-        $(".voted").css("margin-top","0");
-        $(".voting-icons-mediumsmall").css("width","100px");
-
-    },function(){
-        $(".voting-icons-mediumsmall").attr('src','img/conservative/badge-icon-mediumsmall.svg');
-        $(".voted").css("margin-top","800px");
-        $(".voting-icons-mediumsmall").css("width","80px");
-    });
-    
-    
-    
-    
-    
-    
-    
-    
+    getpositionActual();
     
 });
 
-
+function getpositionActual(){
+    console.log(document.body.scrollTop);
+    for(var i=0;i<sizeOfScrollDiv;i++){
+        console.log(document.getElementsByClassName('scrollDiv')[i].getBoundingClientRect().top);
+    }
+}
 
 
 
@@ -382,4 +270,9 @@ function updateComparingContent(){
     }else{
         $("#contentTopic").html(messageComparing);
     }
+}
+
+window.onbeforeunload = function(){
+    $('body').stop(); 
+    window.scrollTo(0,0); 
 }
