879dc9ca19a29570c99a3bf3d6f18970f1b575e4
YOS <yuki8963@gmail.com>
2017-03-26 03:06:02 -0700

    Improved scroll event for homepage

--- index.php
+++ index.php
@@ -8,22 +8,54 @@
                     HOME PAGE RANDOM VIDEOS 
         =========================================================== -->
     
-    <div class="column large-12 show-for-large 
+    <div class="column large-12 medium-12 small-12 
          padding-none center video-container">
         
         <?php 
-            $videoParties = array("VIDEO-01","VIDEO-02","VIDEO-03","VIDEO-04");
+            $videoParties = array("barcelona","liberal","motivational","short");
             $randomVideo = $videoParties[array_rand($videoParties)];
         ?>
         
         <video id="my-video" class="video-homepage" autoplay loop muted>
-            
             <source src="video/<?php echo $randomVideo ?>.mp4" type="video/mp4" />
-            <source src="video/<?php echo $randomVideo ?>.ogg" type="video/ogg" />
-            <source src="video/<?php echo $randomVideo ?>.webm" type="video/webm" />
             Your browser does not support this video 
         </video>
     </div>
+    
+    
+    <div class="column large-12 medium-12 large-12 padding-vertical center" id="subscribe-box">
+        <div class="column large-4 show-for-large padding-none center ">
+            <h4> Keep Yourself Updated with the Voting Whatever! </h4>
+        
+        </div>
+        
+        <div class="column large-8 medium-12 padding-none center ">
+
+            <form method='post' action='' >
+
+                <ul class="subscription center">
+                    <li>
+                        <label for="theName"> Name </label>
+                        <input type="text" name="name" id="theName"
+                               />     
+                    </li>
+                    <li>
+                        <label for="theEmail"> Email </label>
+                        <input type="text" name="email" id="theEmail"
+                               />     
+                    </li>
+                    <li>
+                        <input type="submit" name="subscribe" value="Subscribe"
+                               class="button"/>     
+                    </li>
+
+            </form>            
+        </div>
+
+        
+    </div>
+    
+    
     
     
     
@@ -34,27 +66,26 @@
         =========================================================== -->
     
     
-    <div id="scroll1" class="homepage1">
+    <div id="scroll1" class="homepage1 scrollDiv">
        
-        <div class="row large-12 medium-12 small-12 padding-none center 
-             homepage-desc pos-relative">
-            <div class="column large-12 medium-6 small-12 padding-none center medium-top-style"> 
+        <div class="row large-12 medium-12 small-12 padding-none center homepage-desc">
+            <div class="column large-12 medium-6 small-12 padding-none center"> 
                 <img src="img/homepage/logo-white.png" 
                      alt="the mark your vote up logo" class="logo">
 
             </div>
             
-            <div class="column large-12 medium-6 small-12 padding-none medium-top-style"> 
-                <p class="column">
+            <div class="column large-12 medium-6 small-12 padding-none"> 
+            <p>
                 Chicory arabica, in breve pumpkin spice cream cappuccino.<br>
-                Decaffeinated, white doppio organic crema, 
+                Decaffeinated, white doppio organic crema, <br>
                 frappuccino a id viennese pumpkin spice.<br>
-                Doppio spoon  pot blue 
+                Doppio spoon steamed mocha plunger pot blue <br>
                 mountain kopi-luwak to go chicory arabica.<br>
-                Milk, caramelization, americano so variety,
-                seasonal instant single origin.<br>
-                Extraction, percolator, cinnamon that,
-                as mug aroma macchiato dark.</p>
+                Milk, caramelization, americano so variety,<br>
+                seasonal instant skinny at affogato trifecta single origin.<br>
+                Extraction, percolator, cinnamon that, <br>
+                as mug aroma plunger pot macchiato dark.</p>
            
             <a href="#scroll2" class="show-for-large"> 
                 <img src="img/homepage/homepage-arrow.png" alt="animated arrow" 
@@ -63,12 +94,12 @@
             
             </div>
             
-<!--            <div class="column large-12 medium-12 small-12 padding-none"> 
+            <div class="column large-12 medium-12 small-12 padding-none"> 
                 <a href="#scroll2" class="show-for-medium-only"> 
                     <img src="img/homepage/homepage-arrow.png" alt="animated arrow" 
                     class="arrow" id="arrow"> 
                 </a>
-            </div>-->
+            </div>
         </div>
     </div>
     
@@ -80,29 +111,22 @@
     
     
 
-    <div id="scroll2" class="homepage2 homepage-parties">
+    <div id="scroll2" class="homepage2 homepage-parties scrollDiv">
         <div class="column large-3 show-for-large padding-none center homepage-party">
         
         </div>
     
-        <a href="party.php"> <div class="column large-3 medium-6 small-12 
-                                  padding-vertical homepage-orange center">
+        <div class="column large-3 medium-6 small-12 padding-vertical homepage-orange center">
             <div class="column homepage-orange-hover party-div-height center">
-                <h2> New Democratic Party </h2>
-                <h1> ' Ready for Change '</h1> 
+                <h2> New Democratic </h2>
+                <h3> Ready for Change </h3> 
             </div>
             <div class="column" >
-                <div class="column">
-                    <img src="img/homepage/democratic.png" alt="the logo" 
-                     class="symbol" id="logo-democratic">                
-                </div>
-                <div class="column">
-                    <button class="arrow-parties arrow-democratic"> Learn More </button>
-                </div>
+                <img src="img/homepage/democratic.png" alt="the logo" 
+                 class="symbol">
             </div>
             
-            
-        </div> </a>
+        </div>
         
         
         
@@ -113,22 +137,26 @@
         <div class="column large-3 show-for-large padding-none center homepage-party">
         
         </div>
-        <a href="party.php"> <div class="column large-3 medium-6 small-12 padding-none center homepage-blue">
-            <div class="column homepage-blue-hover party-div-height center">
-                <h2> Conservative Party </h2>
-                <h1> ' Proven Leadership for a Strong Canada '</h1> 
+        <div class="column large-3 medium-6 small-12 padding-none center homepage-blue">
+            <div class="homepage-blue-hover party-div-height">
+                <p> Decaffeinated mug aromatic espresso 
+                    lungo café au lait skinny. Fair trade
+                    americano mug caffeine café au lait 
+                    grinder mug breve. Single shot to go ut,
+                    sit java percolator medium robust 
+                    caramelization dark flavour.
+                    Decaffeinated mug aromatic espresso 
+                    lungo café au lait skinny. Fair trade
+                    americano mug caffeine café au lait 
+                    grinder mug breve. Single shot to go ut,
+                    sit java percolator medium robust 
+                    caramelization dark flavour. </p>
             </div>
-            <div class="column center">
-                <div class="column">
+            <div>
                 <img src="img/homepage/conservative.png" alt="the logo" 
-                     class="symbol" id="logo-conservative">
-                </div>
-                <div class="column">
-                    <button class="arrow-parties arrow-conservative"> Learn More </button>
-                </div>
+                 class="symbol">
             </div>
-            
-        </div> </a>
+        </div>
         
         
         
@@ -136,21 +164,26 @@
         
         
         
-        <a href="party.php"> <div class="column large-3 medium-6 small-12 padding-none center homepage-green">
-            <div class="column homepage-green-hover party-div-height center">
-                <h2> Green Party </h2>
-                <h1> ' A Canada That Works.<br> Together. '</h1> 
+        <div class="column large-3 medium-6 small-12 padding-none center homepage-green">
+            <div class="homepage-green-hover party-div-height">
+                <p> Decaffeinated mug aromatic espresso 
+                    lungo café au lait skinny. Fair trade
+                    americano mug caffeine café au lait 
+                    grinder mug breve. Single shot to go ut,
+                    sit java percolator medium robust 
+                    caramelization dark flavour.
+                    Decaffeinated mug aromatic espresso 
+                    lungo café au lait skinny. Fair trade
+                    americano mug caffeine café au lait 
+                    grinder mug breve. Single shot to go ut,
+                    sit java percolator medium robust 
+                    caramelization dark flavour. </p>
             </div>
             <div>
-                <div class="column">
-                    <img src="img/homepage/green.png" alt="the logo" 
-                 class="symbol" id="logo-green">
-                </div>
-                <div class="column">
-                    <button class="arrow-parties arrow-green"> Learn More </button>
-                </div>
+                <img src="img/homepage/green.png" alt="the logo" 
+                 class="symbol">
             </div>
-        </div> </a>
+        </div>
         <div class="column large-3 show-for-large padding-none center homepage-party">
             
         </div>
@@ -158,25 +191,32 @@
         
         
         
-        <a href="party.php"> <div class="column large-3 medium-6 small-12 padding-none center homepage-red">
-            <div class="column homepage-red-hover party-div-height center">
-                <h2> Liberal Party </h2>
-                <h1> ' Real Change '</h1> 
+        <div class="column large-3 medium-6 small-12 padding-none center homepage-red">
+            <div class="homepage-red-hover party-div-height">
+                <p> Decaffeinated mug aromatic espresso 
+                    lungo café au lait skinny. Fair trade
+                    americano mug caffeine café au lait 
+                    grinder mug breve. Single shot to go ut,
+                    sit java percolator medium robust 
+                    caramelization dark flavour.
+                    Decaffeinated mug aromatic espresso 
+                    lungo café au lait skinny. Fair trade
+                    americano mug caffeine café au lait 
+                    grinder mug breve. Single shot to go ut,
+                    sit java percolator medium robust 
+                    caramelization dark flavour. </p>
             </div>
             <div>
-                <div class="column">
-                    <img src="img/homepage/liberal.png" alt="the logo" 
-                 class="symbol" id="logo-liberal">
-                </div>
-                <div class="column">
-                    <button class="arrow-parties arrow-liberal"> Learn More </button>
-                </div>
+                <img src="img/homepage/liberal.png" alt="the logo" 
+                 class="symbol">
             </div>
-        </div> </a>
+        </div>
         <div class="column large-3 show-for-large padding-none center homepage-party">
         
         </div>
-   
+
+        I ADDED NEW STUFF ON HOMEPAGE
+        
     </div>
 
 
